2cb901eeb6d982efbdc838fd0cd599ec835a8b40
jakub.flis <JakubFlis@users.noreply.github.com>
2018-08-27 12:24:06 +0200

    Test

---
+++ notebooks/Users/jakubf@kainos.com/DevelopmentNotebook.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+import trainmodel
+
+# COMMAND ----------
+
+# Parametrization
+databaseName = ""
+tableName = ""
+tableIdentifier = ""
+outputDir = ""
+tempDir = ""
+
+# COMMAND ----------
+
+# Chain mechanism 
+
+# COMMAND ----------
+
+# Performance analysis
+import time
+time.sleep(0.3)
+
+# COMMAND ----------
+
+# Visual troubleshooting
+
+# COMMAND ----------
+
+trainmodel.train_model()
